8b871c7f33809d110e1606e281816a48735c892d
rmoutard <raphael.moutard@gmail.com>
2013-04-09 17:14:29 +0200

    C: free linked list and add elements

--- c/linked_list.c
+++ c/linked_list.c
@@ -1,19 +1,49 @@
 #include <stdlib.h> // needed for malloc declaration.
+#include <stdio.h>  // needed for printf
 
 // Define a type for this struct, to avoid to long notation
-// struct node * n = NULL;
+// struct linked_list * n = NULL;
 // instead
-// Node * n = NULL;
-typedef struct {
+// LinkedList * n = NULL;
+typedef struct linked_list {
   int value;
-  struct LinkedList * next;
+  struct linked_list * next;
 } LinkedList;
 
+LinkedList * add_value(LinkedList *l, int value){
+  LinkedList *newElement = malloc(sizeof(LinkedList));
+  newElement->value = value;
+  newElement->next = l;
+  return newElement;
+};
+
+void print_linked_list(LinkedList *l){
+  LinkedList * p = l;
+  while(p!=NULL){
+    printf("%d", p->value);
+    p = p->next;
+  }
+};
+
+void free_linked_list(LinkedList *l){
+  LinkedList * currentNode = l;
+  LinkedList * temp = NULL;
+  while(currentNode!=NULL){
+    temp = currentNode;
+    currentNode = currentNode->next;
+    free(temp);
+  }
+  l = NULL;
+};
 
 int main(){
   // A malloc so you need a free.
-  LinkedList *n = malloc(sizeof(LinkedList));
+  LinkedList *n = NULL;
+  n = add_value(n, 5);
+  n = add_value(n, 6);
+  n = add_value(n, 7);
 
-  free(n);
+  print_linked_list(n);
+  free_linked_list(n);
   return 0;
 };
