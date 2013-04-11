6c971a97ad70a7901defc4fa74109b8afefa472c
Raphael MOUTARD <rmoutard@ltutech.com>
2013-04-11 15:49:47 +0200

    Pyhton| add exercise description

--- python/intersection/intersection.py
+++ python/intersection/intersection.py
@@ -1,3 +1,22 @@
+"""Challenge Description:
+
+You are given two sorted list of numbers(ascending order). The lists themselves are comma delimited and the two lists are semicolon delimited. Print out the intersection of these two sets.
+
+Input sample:
+
+File containing two lists of ascending order sorted integers, comma delimited, one per line. e.g.
+
+1,2,3,4;4,5,6
+7,8,9;8,9,10,11,12
+Output sample:
+
+Print out the ascending order sorted intersection of the two lists, one per line
+e.g.
+
+4
+8,9
+"""
+
 def explode(sInput):
   return [int(number) for number in sInput.split(',')]
 
