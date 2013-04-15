829c1375e66a27787a99279c93d90161aea35615
Raphael MOUTARD <rmoutard@ltutech.com>
2013-04-15 16:37:15 +0200

    Cpp| codeEval jollyjumper simple version

--- cpp/jollyjumper/jolly_jumper.cpp
+++ cpp/jollyjumper/jolly_jumper.cpp
@@ -1,4 +1,53 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>      // std::istringstream
+#include <stdlib.h>     /* atoi abs*/
+using namespace std;
+/**
+ * split a line at the space position.
+ */
+bool parseLineInVector(string s, vector<int> & v){
 
-bool isJollyJumper(const char * l) {
+  istringstream ss( s );
+  while (!ss.eof()) {
+    string x;               // here's a nice, empty string
+    getline( ss, x, ' ' );  // try to read the next field into it
+
+    if(x != "" && x != " "){
+      int value = atoi(x.c_str());
+      v.push_back(value);
+    }
+    cout << x << endl;      // print it out, even if we already hit EOF
+  }
   return true;
 }
+
+bool isJollyJumper(const vector<int> & v) {
+  // Problem if v.size() is very big.
+  vector<int> difference(v.size() + 1, 0);
+  difference[0] = 1;
+  for(int i = 0; i < v.size(); ++i){
+    difference[abs(v[i] - v[i+1])] += 1;
+  }
+  for(vector<int>::iterator it = difference.begin(); it < v.end(); ++it){
+    if(*it != 1){
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ A sequence of n > 0 integers is called a jolly jumper if the absolute values of the differences between successive elements take on all possible values 1 through n - 1. eg.
+
+1 4 2 3
+is a jolly jumper, because the absolute differences are 3, 2, and 1, respectively. The definition implies that any sequence of a single integer is a jolly jumper. Write a program to determine whether each of a number of sequences is a jolly jumper.
+Input sample:
+
+Your program should accept as its first argument a path to a filename. Each line in this file is one test case. Each test case will contain an integer n < 3000 followed by n integers representing the sequence. The integers are space delimited.
+
+Output sample:
+
+For each line of input generate a line of output saying 'Jolly' or 'Not jolly'.
+*/
--- cpp/jollyjumper/jolly_jumper_tests.cpp
+++ cpp/jollyjumper/jolly_jumper_tests.cpp
@@ -1,16 +1,60 @@
 #include "jolly_jumper.cpp"
 #include <gtest/gtest.h>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 TEST(JollyJumperTest, OnlyOneElement) {
-  char line[2];
-  strcpy(line, "1");
-  ASSERT_TRUE(isJollyJumper(line));
+  string line("1");
+  vector<int> v;
+  parseLineInVector(line, v);
+  EXPECT_EQ(1, v[0]);
 }
 
 TEST(JollyJumperTest, TwoElements) {
-  char line[4];
-  strcpy(line, "1 3");
-  ASSERT_TRUE(isJollyJumper(line));
+  string line("1 3");
+  vector<int> v;
+  parseLineInVector(line, v);
+  for(vector<int>::iterator it = v.begin(); it != v.end(); ++it){
+    cout << *it << endl;
+  }
+  EXPECT_EQ(3, v[1]);
+}
+
+TEST(JollyJumperTest, OnlyThreeSpaces) {
+  string line("   ");
+  vector<int> v;
+  parseLineInVector(line, v);
+  for(vector<int>::iterator it = v.begin(); it != v.end(); ++it){
+    cout << *it << endl;
+  }
+  EXPECT_EQ(v.size(), 0);
+}
+
+TEST(JollyJumperTest, TrailingWhiteSpace) {
+  string line("1 3   ");
+  vector<int> v;
+  parseLineInVector(line, v);
+  for(vector<int>::iterator it = v.begin(); it != v.end(); ++it){
+    cout << *it << endl;
+  }
+  EXPECT_EQ(v.size(), 2);
+  EXPECT_EQ(3, v[1]);
+}
+
+TEST(JollyJumperTest, isNotJolly) {
+  string line("1 3");
+  vector<int> v;
+  parseLineInVector(line, v);
+  ASSERT_FALSE(isJollyJumper(v));
+}
+
+TEST(JollyJumperTest, isJolly) {
+  string line("1 4 2 3 ");
+  vector<int> v;
+  parseLineInVector(line, v);
+  ASSERT_TRUE(isJollyJumper(v));
 }
 
 int main(int argc, char **argv) {
