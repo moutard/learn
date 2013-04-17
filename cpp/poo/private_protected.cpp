60418188bb4414949659954106d6166f9c8bca5d
Raphael MOUTARD <rmoutard@ltutech.com>
2013-04-17 10:24:24 +0200

    Cpp| private vs protected

---
+++ cpp/poo/private_protected.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+  private:
+    int _private;
+  protected:
+    int _protected;
+  public:
+  int getProtected(){ return _protected;};
+};
+
+class Derivated : public Base {
+  private:
+    int y;
+  public:
+    void setProtected(int i){_protected = i;} // protected is accessible by the derivated class inside the class.
+};
+
+int main(int agrc, char **argv){
+  Base * a = new Base();
+  Derivated * b = new Derivated();
+
+  // a->_protected; // error you can't access a protected variable outside the class
+  a->getProtected(); // OK uses public access function
+  b->setProtected(3); // OK uses public access function.
+  cout << b->getProtected() << endl;
+  delete a;
+  delete b;
+  return 0;
+};
