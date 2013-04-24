ceff93a15e91b77a8dbb1e5db1c4546fb8202ec5
Raphael MOUTARD <rmoutard@ltutech.com>
2013-04-24 18:46:47 +0200

    Cpp| tree in_order with no recursion

--- cpp/tree/Tree.cpp
+++ cpp/tree/Tree.cpp
@@ -1,112 +1,89 @@
 // An object oriented demonstration of tree traversals.
 
-#include <iostream.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <stack>
 
-class tree_node
-{
-  friend class tree;
+using namespace std;
+
+class TreeNode {
+  friend class Tree;
 
   public:
-    tree_node(char string[20]);
+    TreeNode(int);
 
   private:
-    char string[20];
-    tree_node *left;
-    tree_node *right;
-}
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+};
 
-class tree
-{
+class Tree {
   public:
-    ~tree();
+    Tree(){ root=NULL; }
+    ~Tree();
 
-    void add_to_tree(char string[20]);
+    void add_to_tree(int _value);
     void attach_node();
-    void in_order();
+    void in_order_no_recursion();
     void pre_order();
     void post_order();
 
     //destructor helper function
-    void dispose_of_tree(tree_node *current_node);
+    void dispose_of_tree(TreeNode *current_node);
 
 
   private:
-    tree_node *root;
-}
+    TreeNode *root;
+};
 
 
 
-int main()
-{
- char string[20];
- tree the_tree;
+int main() {
+ int i;
+ Tree * the_tree = new Tree();
 
- the_tree.root = NULL; // initialize root of tree to NULL
 
  cout << "Enter a series of strings to be ordered alphabetically\n"
       << "in a binary search tree. These strings can be last names\n"
       << "or any other strings you wish to enter.\n\n"
       << "Enter a blank string to end.\n\n";
- do
-  {
+/* do {
    cout << "Enter a string: ";
-   cin.get(string, 20);
+   cin.get(i, 20);
    cin.ignore(80,'\n');
-   if(strlen(string) != 0)
+   if(strlen(i) != 0)
     {
-     the_tree.add_to_tree(string);
+     the_tree->add_to_tree(i);
     }
   } while(strlen(string) != 0);
 
  cout << "\nAn in-order traversal of the tree yields:\n";
- the_tree.in_order();
-
- cout << "\nPress Enter to continue.\n";
- cin.get(string, 80);
- cin.ignore(80,'\n');
- cout << "\nA preorder traveral of the tree yields:\n";
- the_tree.pre_order();
-
- cout << "\nPress Enter to continue.\n";
- cin.get(string,80);
- cin.ignore(80,'\n');
- cout << "\nA postorder traveral of the tree yields:\n";
- the_tree.post_order();
-
- cout << "\nPress Enter to continue.\n";
- cin.get(string,80);
- cin.ignore(80,'\n');
-
+ the_tree->in_order();
+*/
  return 0;
 }
 
 
-void tree_node::tree_node(char newstring[20])
-{
+void TreeNode::TreeNode(int _value) {
  // Initialize new node.
- strcpy(string, newstring);
+ value = _value;
  left = NULL;
  right = NULL;
-}
+};
 
 
-tree::~tree()
-{
+Tree::~Tree() {
   dispose_of_tree(root);
 }
 
 
-void tree::dispose_of_tree(tree_node *current_node)
-{
- if(!((current_node->left == NULL) && (current_node->right == NULL)))
-  {
-   if(current_node->right != NULL)
-    {
+void Tree::dispose_of_tree(TreeNode *current_node) {
+ if(!((current_node->left == NULL) && (current_node->right == NULL))) {
+   if(current_node->right != NULL) {
      dispose_of_tree(current_node->right);
     }
-   if(current_node->left != NULL)
-    {
+   if(current_node->left != NULL) {
      dispose_of_tree(current_node->left);
     }
   }
@@ -114,33 +91,27 @@ void tree::dispose_of_tree(tree_node *current_node)
 }
 
 
-void tree::add_to_tree(char string[20])
-{
- tree_node *new_node_ptr;
+void Tree::add_to_tree(int _value) {
+ TreeNode *new_node_ptr;
 
- new_node_ptr = new tree_node(string[20]);
+ new_node_ptr = new TreeNode(_value);
 
- if(root == NULL)
-  {
+ if(root == NULL) {
    root = new_node_ptr;
-  }
- else
-  {
+  } else {
    attach_node(new_node_ptr);
   }
 }
 
 
-void tree::attach_node(tree_node *new_node_ptr)
-{
- tree_node *search_ptr;
- tree_node *follow_ptr;
+void Tree::attach_node(TreeNode *new_node_ptr) {
+ TreeNode *search_ptr;
+ TreeNode *follow_ptr;
 
  search_ptr = root;
  follow_ptr = root;
 
- while(search_ptr != NULL)
-  {
+ while(search_ptr != NULL) {
    follow_ptr = search_ptr;
    if(strcmp(new_node_ptr->string, search_ptr->string) < 0)
     {
@@ -162,40 +133,24 @@ void tree::attach_node(tree_node *new_node_ptr)
 }
 
 
-void tree::in_order()
-{
- tree_node *current_node = root;
+void Tree::in_order_no_recursion() {
+  TreeNode *current_node = root;
+  stack<TreeNode *> s;
+  bool done = false;
 
- if(current_node != NULL)
-  {
-   in_order(current_node->left);
-   cout << current_node->string << endl;
-   in_order(current_node->right);
-  }
-}
-
-
-void tree::pre_order()
-{
- tree_node *current_node = root;
-
- if(current_node != NULL)
-  {
-   cout << current_node->string << endl;
-   pre_order(current_node->left);
-   pre_order(current_node->right);
-  }
-}
-
-
-void tree::post_order()
-{
- tree_node *current_node = root;
-
- if(current_node != NULL)
-  {
-   post_order(current_node->left);
-   post_order(current_node->right);
-   cout << current_node->string << endl;
+  while(!done) {
+    if(current_node){
+      s.push(current_node);
+      current_node = current_node->left;
+    } else {
+      if(s.empty()){
+        done = true;
+      } else {
+        current_node = s.top();
+        s.pop();
+        cout << current_node->value << endl;
+        current_node = current_node->right;
+      }
+    }
   }
 }
