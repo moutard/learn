b0f2b7c35372b111a2bd43d4dc095d1849a9b799
rmoutard <raphael.moutard@gmail.com>
2013-05-08 21:13:59 +0200

    Run tests in the main

---
+++ cpp/raymini/Main.cpp
@@ -0,0 +1,40 @@
+#include <QApplication>
+#include <GLViewer.h>
+#include <Window.h>
+#include <string>
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "QTUtils.h"
+
+using namespace std;
+
+int main (int argc, char **argv) {
+
+  testing::InitGoogleTest(&argc, argv);
+  RUN_ALL_TESTS();
+
+  QApplication raymini (argc, argv);
+  //setBoubekQTStyle (raymini);
+  std::cout<<"QApplication ok"<<std::endl;
+  //QApplication::setStyle (new QPlastiqueStyle);
+  //GLViewer viewer;
+
+    Window * window = new Window ();
+#if QT_VERSION < 0x040000
+
+
+  // Set the viewer as the application main widget.
+  //application.setMainWidget(&viewer);
+#else
+  //viewer.setWindowTitle("simpleViewer");
+#endif
+    window->show();
+  // Make the viewer window visible on screen.
+  //viewer.show();
+
+  // Run main loop.
+  return raymini.exec();
+
+}
+
