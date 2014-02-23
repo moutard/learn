486529803096f77b365ccfeabed36f1667e709bd
rmoutard <raphael.moutard@gmail.com>
2014-02-23 17:06:13 +0000

    CodeEval: optimize levenshtein distance

--- c/levenshtein/levenshtein.c
+++ c/levenshtein/levenshtein.c
@@ -53,8 +53,50 @@ int levenshteinDistance(char * s, char * t) {
   return v1[iTLength];
 };
 
+int optimizeLevenshteinDistance(char * s, char * t) {
+
+  // Degenerate Cases.
+  if (strcmp(s,t)==0) { return 0;}
+  unsigned int iSLength = strlen(s);
+  unsigned int iTLength = strlen(t);
+  if (iSLength == 0) {return iTLength;}
+  if (iTLength == 0) {return iSLength;}
+
+  // Create the two vectors (corresponds to the previous and current line of the matrix)
+  unsigned int v0[iTLength + 1];
+  unsigned int v1[iTLength + 1];
+
+  // Initialize each vector.
+  for (unsigned int i = 0; i < iTLength + 1; ++i) {
+    v0[i] = i;
+  }
+
+  for (unsigned int i = 0; i < iSLength; ++i) {
+    // Calculate v1 (the current row distance)
+    v1[0] = i + 1;
+
+    // Use the formulat to fill in the rest of the row.
+    for (unsigned int j = 0; j < iTLength; ++j) {
+      unsigned int cost = (s[i] == t[j]) ? 0 : 1;
+      v1[j+1] = min(v1[j] + 1, v0[j+1] + 1, v0[j] + cost);
+    }
+
+    // copy v1 (current row) to v0;
+    unsigned int iCurrentMin = iSLength + iTLength;
+    for (unsigned int k = 0; k < iTLength + 1; ++k) {
+      v0[k] = v1[k];
+      if (v0[k] < iCurrentMin) {iCurrentMin = v0[k];}
+    }
+    // If on the line the current min is more than 2 then the distance can only
+    // increase. So it allows to stop before the end of the word.
+    // it's just a samll trick it doesn't decrease complexity.
+    if (iCurrentMin > 1) return 2;
+  }
+  return v1[iTLength];
+};
+
 int main() {
   printf("%i\n", levenshteinDistance("kitten", "sitting"));
-  printf("%i\n", levenshteinDistance("saturday", "sunday"));
+  printf("%i\n", optimizeLevenshteinDistance("saturday", "sunday"));
   return 0;
 }
