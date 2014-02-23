86d3d2d80cc92757ac4c39015016ca9bfb6fae20
rmoutard <raphael.moutard@gmail.com>
2014-02-23 16:30:34 +0000

    CodeEval: levenshtein distance

---
+++ c/levenshtein/levenshtein.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+unsigned int min(unsigned int a, unsigned int b, unsigned int c) {
+  unsigned int min;
+  if (a < b) {
+    min = a;
+  } else {
+    min = b;
+  }
+
+  if (c < min) {
+    return c;
+  } else {
+    return min;
+  }
+};
+
+int levenshteinDistance(char * s, char * t) {
+
+  // Degenerate Cases.
+  if (strcmp(s,t)==0) { return 0;}
+  unsigned int iSLength = strlen(s);
+  unsigned int iTLength = strlen(t);
+  if (iSLength == 0) {return iTLength;}
+  if (iTLength == 0) {return iSLength;}
+
+  // Create the two vectors (corresponds to the previous and current line of the matrix)
+  unsigned int v0[iTLength + 1];
+  unsigned int v1[iTLength + 1];
+
+  // Initialize each vector.
+  for (unsigned int i = 0; i < iTLength + 1; ++i) {
+    v0[i] = i;
+  }
+
+  for (unsigned int i = 0; i < iSLength; ++i) {
+    // Calculate v1 (the current row distance)
+    v1[0] = i + 1;
+
+    // Use the formulat to fill in the rest of the row.
+    for (unsigned int j = 0; j < iTLength; ++j) {
+      unsigned int cost = (s[i] == t[j]) ? 0 : 1;
+      v1[j+1] = min(v1[j] + 1, v0[j+1] + 1, v0[j] + cost);
+    }
+
+    // copy v1 (current row) to v0;
+    for (unsigned int k = 0; k < iTLength + 1; ++k) {
+      v0[k] = v1[k];
+    }
+  }
+  return v1[iTLength];
+};
+
+int main() {
+  printf("%i\n", levenshteinDistance("kitten", "sitting"));
+  printf("%i\n", levenshteinDistance("saturday", "sunday"));
+  return 0;
+}
