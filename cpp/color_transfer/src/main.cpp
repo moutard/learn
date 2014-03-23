b8b8e28858f176b330506d1192238e34149db92a
Raphael Moutard <raphael.moutard@gmail.com>
2014-03-23 01:45:03 +0000

    Cpp: color_transfert

--- cpp/color_transfer/src/main.cpp
+++ cpp/color_transfer/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "transfert.h"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -10,17 +11,47 @@ namespace po = boost::program_options;
 
 int main(int argc, char * argv[])
 {
-  if (argc != 3) {
-    cout << "ERROR - Invalide number of arguments" << endl;
-    cout << "source/file/path color/file/path" << endl;
-    return 2;
+
+  // Declare the supported options.
+  po::options_description desc("Allowed options");
+  string output;
+  int kmeans;
+  desc.add_options()
+    ("help,h", "produce help message")
+    ("input,i", po::value<string>(), "path to the input image")
+    ("color,c", po::value<string>(), "path to the color image")
+    ("output,o", po::value<string>(&output)->default_value("colored.png"), "path to the output image");
+    ("kmeans,k", po::value<int>(&kmeans)->default_value(0), "use kmeans cluster before recoloration [!BETHA can produce some unexpected results]");
+
+  po::variables_map oVMap;
+  po::store(po::parse_command_line(argc, argv, desc), oVMap);
+  po::notify(oVMap);
+
+  if (oVMap.count("help")) {
+    cout << desc << endl;
+    return 1;
   }
+
+  if (!oVMap.count("input")) {
+     cout << "Input image is mandatory." << endl;
+     cout << desc << endl;
+    return 1;
+  }
+  if (!oVMap.count("color")) {
+     cout << "Color image is mandatory." << endl;
+     cout << desc << endl;
+    return 1;
+  }
+
+  cout << "Input set to "
+        << oVMap["input"].as<string>() << ".\n";
+
   /// Create a window that can display an image.
   char window_name[] = "Transfer";
   namedWindow( window_name, WINDOW_AUTOSIZE );
 
   /// Load the source image
-  Mat src = imread(argv[1], 1 );
+  Mat src = imread(oVMap["input"].as<string>(), 1);
   Mat dst = Mat::zeros(src.rows, src.cols, CV_8UC3);
 
   if (src.data == NULL) {
@@ -28,7 +59,7 @@ int main(int argc, char * argv[])
     return 1;
   }
   //Mat clr = imread("leaves-green.jpg", 1);
-  Mat clr = imread(argv[2], 1);
+  Mat clr = imread(oVMap["color"].as<string>() , 1);
   if (clr.data == NULL) {
     cout << "The color image can not be found. Invalid path: " << argv[2] << endl;
     return 1;
@@ -43,6 +74,7 @@ int main(int argc, char * argv[])
   /// Save the result.
   const char * sOutputName = "colored.jpg";
   imwrite(sOutputName, dst);
+
 
   cout << "The result has been saved as" << sOutputName << endl;
   return 0;
