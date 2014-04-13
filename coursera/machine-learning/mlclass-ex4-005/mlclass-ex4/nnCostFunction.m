621ca89568921aab2626d698fc9cea73fb2be639
Raphael Moutard <raphael.moutard@gmail.com>
2014-04-13 14:28:59 +0100

    Coursera::machine_learning cost function regularized

--- coursera/machine-learning/mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
+++ coursera/machine-learning/mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -23,9 +23,9 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
                  num_labels, (hidden_layer_size + 1));
 
 % Setup some useful variables
-m = size(X, 1);
-         
-% You need to return the following variables correctly 
+m = size(X, 1)
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -39,6 +39,33 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
+% refactor labels y (y is a 5000 * 1 size that contains labels.
+% transform into a matrix that contains m vectors (10 * 1)
+Y = zeros(m, num_labels);
+for i=1:m
+  Y(i, y(i)) = 1;
+end
+% a feature x^(i) is a row of X
+
+% add biais unit for each feature.
+% a_1 size (n + 1) * m (features * training set)
+a_1 = [ones(m, 1) X]';
+
+% z_2 size hidden_units * m
+z_2 = Theta1 * a_1;
+
+% add biais unit for each feature.
+% a_2 size
+a_2 = [ones(1, m); sigmoid(z_2)];
+
+z_3 = Theta2 * a_2;
+
+a_3 = sigmoid(z_3);
+
+h_theta = a_3;
+% sum only diagonal elements. Can be improve as we compute a lot of useless element.
+% but I guess that's better than a for loop.
+J = (1/m) * trace( -Y * log(h_theta) - (1 - Y) * log(1 - h_theta)) ;
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -46,12 +73,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,6 +89,11 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+
+% remove the biais before the regularization
+Theta1(:,1) = zeros(hidden_layer_size, 1);
+Theta2(:,1) = zeros(num_labels, 1);
+J = J + (lambda/(2*m)) * (sum(sum(Theta1 .^2)) + sum(sum(Theta2 .^2)));
 
 
 
